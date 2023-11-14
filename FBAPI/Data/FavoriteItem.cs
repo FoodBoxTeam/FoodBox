ac562515b03a20fac5d1550bb2e4a85298f3ede8
Ahmad Jebran <ahmad.jebran@students.snow.edu>
2023-11-14 10:53:28 -0700

    scafolded the data

---
+++ FBAPI/Data/FavoriteItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FBAPI.Data;
+
+public partial class FavoriteItem
+{
+    public int Id { get; set; }
+
+    public int ItemId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public virtual Customer Customer { get; set; } = null!;
+
+    public virtual Item Item { get; set; } = null!;
+}
