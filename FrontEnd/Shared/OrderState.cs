b01844fa8e1c0a50ccc86019649ad3da111bec7b
Cort Cox <cort.cox@students.snow.edu>
2023-11-17 18:48:25 -0700

    Added the ability to order multiple items. Not storing in the database yet.

---
+++ FrontEnd/Shared/OrderState.cs
@@ -0,0 +1,12 @@
+﻿using FrontEnd.Pages;
+using FrontEnd.Pages.Dto;
+
+namespace FrontEnd.Shared
+{
+    public class OrderState
+    {
+        public List<OrderingItemDto> ItemsInOrder { get; set; } = new List<OrderingItemDto>();
+        // public bool HasBeenOrdered { get; set; } = false;
+        // public bool HasBeenPaidFor { get; set; } = false;
+    }
+}
