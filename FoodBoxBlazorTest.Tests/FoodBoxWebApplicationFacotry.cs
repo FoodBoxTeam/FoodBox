148e7140fd8d0a554726ad313cdef08df224eac5
Benson Bird <benson.bird@students.snow.edu>
2023-11-29 17:29:17 -0700

    Progress in tests

---
+++ FoodBoxBlazorTest.Tests/FoodBoxWebApplicationFacotry.cs
@@ -0,0 +1,48 @@
+﻿using FrontEnd;
+using FrontEnd.Data;
+using Humanizer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+
+namespace FoodBoxBlazorTest.Tests
+{
+    public class FoodBoxWebApplicationFacotry : WebApplicationFactory<Program>
+    {
+        private readonly PostgreSqlContainer _dbContainer;
+
+        public FoodBoxWebApplicationFacotry()
+        {
+            var backupFile = Directory.GetFiles("../../../..", "*.sql", SearchOption.AllDirectories)
+                .Select(f => new FileInfo(f))
+                .OrderByDescending(fi => fi.LastWriteTime)
+                .First();
+
+            _dbContainer = new PostgreSqlBuilder()
+              .WithImage("postgres")
+              .WithPassword("P@ssword!")
+              .WithBindMount(backupFile.FullName, "/docker-entrypoint-initdb.d/init.sql")
+              .Build();
+        }
+
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<FoodBoxDB>));
+                services.AddDbContext<FoodBoxDB>(options => options.UseNpgsql(_dbContainer.GetConnectionString()));
+            });
+        }
+    }
+}
--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -5,68 +5,76 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-//var connectionString = builder.Configuration.GetConnectionString("FoodBoxDBConnection") ?? throw new InvalidOperationException("Connection string 'FoodBoxDBConnection' not found.");
-builder.Services.AddDbContextFactory<FoodBoxDB>(options => options.UseNpgsql((builder.Configuration["db"])));
+namespace FrontEnd;
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddDefaultUI()
-    .AddDefaultTokenProviders()
-    .AddEntityFrameworkStores<FoodBoxDB>();
-
-
-builder.Services.AddAuthentication()
-   .AddGoogle(options =>
-   {
-       IConfigurationSection googleAuthNSection =
-       builder.Configuration.GetSection("Authentication:Google");
-       options.ClientId = googleAuthNSection["ClientId"];
-       options.ClientSecret = googleAuthNSection["ClientSecret"];
-   });
-//.AddFacebook(options =>
-//{
-//    IConfigurationSection FBAuthNSection =
-//    config.GetSection("Authentication:FB");
-//    options.ClientId = FBAuthNSection["ClientId"];
-//    options.ClientSecret = FBAuthNSection["ClientSecret"];
-//})
-//.AddMicrosoftAccount(microsoftOptions =>
-//{
-//    microsoftOptions.ClientId = config["Authentication:Microsoft:ClientId"];
-//    microsoftOptions.ClientSecret = config["Authentication:Microsoft:ClientSecret"];
-//})
-//.AddTwitter(twitterOptions =>
-//{
-//    twitterOptions.ConsumerKey = config["Authentication:Twitter:ConsumerAPIKey"];
-//    twitterOptions.ConsumerSecret = config["Authentication:Twitter:ConsumerSecret"];
-//    twitterOptions.RetrieveUserDetails = true;
-//});
-
-// Add services to the container.
-builder.Services.AddSingleton<OrderState>();
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<StateContainer>();
-builder.Services.AddHostedService<DefaultUserService>();
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        //var connectionString = builder.Configuration.GetConnectionString("FoodBoxDBConnection") ?? throw new InvalidOperationException("Connection string 'FoodBoxDBConnection' not found.");
+        builder.Services.AddDbContextFactory<FoodBoxDB>(options => options.UseNpgsql((builder.Configuration["db"])));
+
+        builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddDefaultUI()
+            .AddDefaultTokenProviders()
+            .AddEntityFrameworkStores<FoodBoxDB>();
+
+
+        builder.Services.AddAuthentication()
+           .AddGoogle(options =>
+           {
+               IConfigurationSection googleAuthNSection =
+               builder.Configuration.GetSection("Authentication:Google");
+               options.ClientId = googleAuthNSection["ClientId"];
+               options.ClientSecret = googleAuthNSection["ClientSecret"];
+           });
+        //.AddFacebook(options =>
+        //{
+        //    IConfigurationSection FBAuthNSection =
+        //    config.GetSection("Authentication:FB");
+        //    options.ClientId = FBAuthNSection["ClientId"];
+        //    options.ClientSecret = FBAuthNSection["ClientSecret"];
+        //})
+        //.AddMicrosoftAccount(microsoftOptions =>
+        //{
+        //    microsoftOptions.ClientId = config["Authentication:Microsoft:ClientId"];
+        //    microsoftOptions.ClientSecret = config["Authentication:Microsoft:ClientSecret"];
+        //})
+        //.AddTwitter(twitterOptions =>
+        //{
+        //    twitterOptions.ConsumerKey = config["Authentication:Twitter:ConsumerAPIKey"];
+        //    twitterOptions.ConsumerSecret = config["Authentication:Twitter:ConsumerSecret"];
+        //    twitterOptions.RetrieveUserDetails = true;
+        //});
+
+        // Add services to the container.
+        builder.Services.AddSingleton<OrderState>();
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddScoped<StateContainer>();
+        builder.Services.AddHostedService<DefaultUserService>();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
