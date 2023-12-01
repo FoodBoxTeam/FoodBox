0b2e5de79f09a30a2cd90a17a48b2131e968217d
Cort Cox <cort.cox@students.snow.edu>
2023-12-01 16:44:37 -0700

    UpToDate

---
+++ FrontEnd/Data/Cart.cs
@@ -0,0 +1,13 @@
+﻿namespace FrontEnd.Data;
+
+public class Cart
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public int RestaurantId { get; set; }
+
+    public virtual Customer Customer { get; set; } = new Customer();
+    public virtual Restaurant Restaurant { get; set; } = new Restaurant();
+    public virtual IEnumerable<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+}
