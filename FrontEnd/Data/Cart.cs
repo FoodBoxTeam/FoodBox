a866fc3f9a7c019c418a66cd54ae3a7f034fd61f
BensonB12 <89985506+BensonB12@users.noreply.github.com>
2023-12-07 01:44:00 -0700

    On Parameter set, Delete all items in cart, contact us, Location is change-able

--- FrontEnd/Data/Cart.cs
+++ FrontEnd/Data/Cart.cs
@@ -6,7 +6,7 @@ public class Cart
     public int CustomerId { get; set; }
     public int RestaurantId { get; set; }
     public virtual Customer Customer { get; set; } = null!;
-    public virtual Restaurant Restaurant { get; set; } = null!;
+    public virtual Restaurant? Restaurant { get; set; }
     public virtual IEnumerable<CartItem> CartItems { get; set; } = new List<CartItem>();
 
 }
