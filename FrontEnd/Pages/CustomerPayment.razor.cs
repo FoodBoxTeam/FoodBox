90bc2fdb04f27cce0616ef7fcfedc840a95c37e0
Benson Bird <benson.bird@students.snow.edu>
2023-12-07 11:16:39 -0700

    All Better

---
+++ FrontEnd/Pages/CustomerPayment.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.Pages
+{
+    [Serializable]
+    public class Payment
+    {
+        [Required(ErrorMessage = "Pick up method is required.")]
+        public bool IsDelivery {  get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Card Number is required.")]
+        public string CardNumber { get; set; }
+        [Required(ErrorMessage = "Experation date is required.")]
+        public DateOnly ExpirationDate { get; set; }
+        [Required(ErrorMessage = "CVV is required.")]
+        public int Cvv { get; set; }
+        [Required(ErrorMessage = "Address Is Requred if you are doing a delivery")]
+        public string Address { get => IsDelivery ? Address : "" ; set => Address = value; }
+    }
+}
